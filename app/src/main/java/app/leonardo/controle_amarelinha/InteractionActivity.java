b417a5766854a970257e09c11fe9c60777cba133
Leonardo Everson <leonardo.everson@outlook.com>
2017-10-11 17:41:43 -0300

    pequenas implementações

--- app/src/main/java/app/leonardo/controle_amarelinha/InteractionActivity.java
+++ app/src/main/java/app/leonardo/controle_amarelinha/InteractionActivity.java
@@ -1,13 +1,26 @@
 package app.leonardo.controle_amarelinha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InteractionActivity extends AppCompatActivity {
-
+    public Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interaction);
+
+        button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InteractionActivity.this,ActionActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
